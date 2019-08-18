0ec00682b6500cf52b2e63a446166149c10e869a
John Otu <johnne01@gmail.com>
2019-08-18 15:00:24 +0100

    separated operands and operators
    so they can be handled together during compute

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let calcInputs = "";
-const screenContents = [];
+let screenContents = [];
+let operands = [];
+let operators = [];
 
-const display = document.getElementById('disp');
+let display = document.getElementById('disp');
 
 display.innerText = '0';
 
@@ -13,14 +15,18 @@ display.innerText = '0';
 
 function prepInput(input) {
   if (input === 'deci') {
-    if (calcInputs.includes('.')){
-      return;
-    }
+    if (calcInputs.includes('.')) return;
     if (calcInputs.length > 0) {
       handleInput('.');
     } else {
       handleInput('0.');
     }
+  } else if (input.match(/[\/*+-]/) ) {
+    console.log('operator matched');
+    operands.push(calcInputs);
+    operators.push(input);
+    partialClear();
+    console.log('operands', operands, '\operators', operators);
   } else {
     handleInput(input);
   }
@@ -32,3 +38,19 @@ function handleInput(input) {
   display.innerText = screenContents.join('');
   console.log('input', calcInputs);
 }
+
+function fullClear() {
+  calcInputs = "";
+  screenContents = [];
+  operands = [];
+  operators = [];
+  display.innerText = '0';
+  console.log('operands', operands, '\operators', operators);
+}
+
+function partialClear() {
+  calcInputs = "";
+  screenContents = [];
+  display.innerText = '0';
+}
+
