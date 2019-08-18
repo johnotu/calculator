9aba484fa31e044619f333077dfe5ec061392258
John Otu <johnne01@gmail.com>
2019-08-18 13:46:23 +0100

    handled input display
    also handle correct display of decimal at the beginning or within input

---
+++ js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let calcInputs = [];
+const screenContents = [];
+
+const display = document.getElementById('disp');
+
+display.innerText = '0';
+
+
+
+
+
+function prepInput(input) {
+  if (input === 'deci') {
+    let deci = 0;
+    if (calcInputs.length > 0) {
+      handleInput('.');
+    } else {
+      handleInput('0.');
+    }
+  } else {
+    handleInput(input);
+  }
+}
+
+function handleInput(input) {
+  calcInputs += input;
+  screenContents.push(input);
+  display.innerText = screenContents.join('');
+  console.log('input', calcInputs);
+}
